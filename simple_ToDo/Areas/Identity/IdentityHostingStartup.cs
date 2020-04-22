46c3784dd47dceb59f5c658744697cbf2b431c0c
jamesnitz <jamesanitz2@gmail.com>
2020-04-22 13:35:57 -0500

    first commit

---
+++ simple_ToDo/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using simple_ToDo.Data;
+using simple_ToDo.Models;
+
+[assembly: HostingStartup(typeof(simple_ToDo.Areas.Identity.IdentityHostingStartup))]
+namespace simple_ToDo.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ simple_ToDo/Models/TodoStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace simple_ToDo.Models
+{
+    public class TodoStatus
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+    }
+}
